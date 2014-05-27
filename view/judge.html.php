37fa147fead4f6079840ecdf2ac06a19db8be24a
ZhuGuangwen <zhuguangwen@summer.local>
2014-05-27 10:13:30 +0800

    add judge

---
+++ view/judge.html.php
@@ -0,0 +1,16 @@
+<html>
+<?php include("head.html"); ?>
+<body>
+<div style="width: 800px;margin: 0 auto;">
+
+    <div style="width: 800px;margin: 0 auto;">
+        <h1>在线测试</h1>
+        <form method="post" action="../handler/program_judge.php">
+        <input type="submit" value="提交">
+        <textarea  style="width:800px;max-width: 800px;height:500px;" name="code"></textarea>
+        </form>
+    </div>
+
+</div>
+</body>
+</html>
